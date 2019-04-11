94dfbb676ca4167162e206071c923691ab6ebff0
Erwan HAQUET <ehaquet.dev@gmail.com>
2019-04-11 20:46:07 +0200

    Update services repositories 'TwitchScrutinier'=> 'TILER'

--- docker-init.sh
+++ docker-init.sh
@@ -5,11 +5,11 @@ if [ ! -f ".env" ]; then
 fi
 
 if [ ! -d "orchestrator" ]; then
-  git clone git@github.com:erwan-haquet/twitch-scrutinizer-orchestrator.git orchestrator
+  git clone git@github.com:erwan-haquet/tiler-orchestrator.git orchestrator
 fi
 
 if [ ! -d "observer" ]; then
-  git clone git@github.com:erwan-haquet/twitch-scrutinizer-observer.git observer
+  git clone git@github.com:erwan-haquet/tiler-observer.git observer
 fi
 
 docker-compose build
